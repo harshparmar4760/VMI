c9f1eb0bad0fc0eabe72573410d0d7b770e675b0
harshparmar4760 <harshparmar4760@gmail.com>
2023-03-05 13:33:24 +0530

    save

--- script.js
+++ script.js
@@ -13,3 +13,43 @@ if (close) {
         nav.classList.remove('active');
     })
 }
+
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 100;
+
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+  
+
+
+
+// transition for products
+
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         console.log(entry)
+//         if (entry.isIntersecting){
+//             entry.target.classList.add('show');
+//         } else{
+//             entry.target.classList.remove('show');
+//         }
+//     })
+// })
+
+//     const hiddenElements = document.querySelectorAll('.hidden');    
+//     hiddenElements.forEach((el) => observer.observe(el));
+    
+ 
