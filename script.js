b183e327530f1266cdd8fd2918fc35ebfbea3b28
harshparmar4760 <harshparmar4760@gmail.com>
2023-05-07 11:55:41 +0530

    save

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function reveal() {
     for (var i = 0; i < reveals.length; i++) {
       var windowHeight = window.innerHeight;
       var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 100;
+      var elementVisible = 80;
 
       if (elementTop < windowHeight - elementVisible) {
         reveals[i].classList.add("active");
